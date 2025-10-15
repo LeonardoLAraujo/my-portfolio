e7621bc8f50bcab0c286a706ef6689589e4b0753
LeonardoLAraujo <leonardoleal018@gmail.com>
2025-10-15 15:53:14 -0300

    Update contact

--- api/contact.ts
+++ api/contact.ts
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
     if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
         throw new Error("FIREBASE_SERVICE_ACCOUNT environment variable is not set");
     }
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 }
 
